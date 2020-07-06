be8efd46f5b514950ab1f07a397e37aee119f994
Wayyan <wayyan@devduo.org>
2020-07-06 10:54:02 +0630

    Initial Commit

---
+++ ToasterLibrary/src/main/java/com/devduo/toasterlibrary/ToasterMessage.kt
@@ -0,0 +1,10 @@
+package com.devduo.toasterlibrary
+
+import android.content.Context
+import android.widget.Toast
+
+class ToasterMessage {
+    public fun s(c: Context, message: String) {
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
+    }
+}
